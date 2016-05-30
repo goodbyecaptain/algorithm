d85543eb264736b51948bdc2fffcbf85f72600ab
Kevin Qian <scifi.org@gmail.com>
2016-05-30 11:24:34 +0800

    remove berkley

--- berkeley_cs61b/SimpleIO.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-class SimpleIO {
-	public static void main(String[] args) throws Exception {
-		BufferedReader keybd = 
-			new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(keybd.readLine());
-	}
-}
